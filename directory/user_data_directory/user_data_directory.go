6181d240ab43cfbdc92eeaf4385cf4e2fde14fa1
Benjamin Borbe <bborbe@rocketnews.de>
2016-05-13 10:12:40 +0200

    implement user data store

--- directory/user_data_directory/user_data_directory.go
+++ directory/user_data_directory/user_data_directory.go
@@ -13,7 +13,7 @@ var logger = log.DefaultLogger
 const PREFIX = "user_data"
 
 type directory struct {
-	ledis ledis.Kv
+	ledis ledis.Hash
 }
 
 type UserDataDirectory interface {
@@ -25,42 +25,53 @@ type UserDataDirectory interface {
 	DeleteValue(userName api.UserName, key string) error
 }
 
-func New(ledisClient ledis.Kv) *directory {
+func New(ledisClient ledis.Hash) *directory {
 	a := new(directory)
 	a.ledis = ledisClient
 	return a
 }
 
-func createKey(applicationName api.ApplicationName) string {
-	return fmt.Sprintf("%s:%s", PREFIX, applicationName)
+func createKey(userName api.UserName) string {
+	return fmt.Sprintf("%s:%s", PREFIX, userName)
 }
 
 func (d *directory) Set(userName api.UserName, data map[string]string) error {
 	logger.Debugf("set %v for user %v %v", data, userName)
+	key := createKey(userName)
+	for k, v := range data {
+		if err := d.ledis.HashSet(key, k, v); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
-func (d *directory) SetValue(userName api.UserName, key string, value string) error {
-	logger.Debugf("set %s=%s for user %v", key, value, userName)
-	return nil
+func (d *directory) SetValue(userName api.UserName, field string, value string) error {
+	logger.Debugf("set %s=%s for user %v", field, value, userName)
+	key := createKey(userName)
+	return d.ledis.HashSet(key, field, value)
 }
 
 func (d *directory) Get(userName api.UserName) (map[string]string, error) {
 	logger.Debugf("get data of user %v", userName)
-	return nil, nil
+	key := createKey(userName)
+	return d.ledis.HashGetAll(key)
 }
 
-func (d *directory) GetValue(userName api.UserName, key string) (string, error) {
-	logger.Debugf("get %s of user %v", key, userName)
-	return "", nil
+func (d *directory) GetValue(userName api.UserName, field string) (string, error) {
+	logger.Debugf("get %s of user %v", field, userName)
+	key := createKey(userName)
+	return d.ledis.HashGet(key, field)
 }
 
 func (d *directory) Delete(userName api.UserName) error {
 	logger.Debugf("delete data of user %v", userName)
-	return nil
+	key := createKey(userName)
+	return d.ledis.HashClear(key)
 }
 
-func (d *directory) DeleteValue(userName api.UserName, key string) error {
-	logger.Debugf("delete %s of user %v", key, userName)
-	return nil
+func (d *directory) DeleteValue(userName api.UserName, field string) error {
+	logger.Debugf("delete %s of user %v", field, userName)
+	key := createKey(userName)
+	return d.ledis.HashDel(key, field)
 }
