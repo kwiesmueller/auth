d143f6bbd0c73c39d4e6426dc7d8ce415f433caa
Benjamin Borbe <bborbe@rocketnews.de>
2016-09-02 11:27:36 +0200

    remove debug flag

--- bin/auth_server/auth_server_test.go
+++ bin/auth_server/auth_server_test.go
@@ -7,14 +7,14 @@ import (
 )
 
 func TestServerFails(t *testing.T) {
-	_, err := createServer(0, false, "/auth", "S3CR3T", "ledisdb:1337", "S3CR3T")
+	_, err := createServer(0, "/auth", "S3CR3T", "ledisdb:1337", "S3CR3T")
 	if err := AssertThat(err, NotNilValue()); err != nil {
 		t.Fatal(err)
 	}
 }
 
 func TestServerSuccess(t *testing.T) {
-	srv, err := createServer(1337, false, "/auth", "S3CR3T", "ledisdb:1337", "S3CR3T")
+	srv, err := createServer(1337, "/auth", "S3CR3T", "ledisdb:1337", "S3CR3T")
 	if err := AssertThat(err, NilValue()); err != nil {
 		t.Fatal(err)
 	}
