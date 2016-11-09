cc3c47aaa26e8a1180a13c00b43dda36861cd002
Benjamin Borbe <bborbe@rocketnews.de>
2016-11-09 21:35:31 +0100

    update logging

--- service/application/auth_application_service.go
+++ service/application/auth_application_service.go
@@ -8,16 +8,16 @@ import (
 	"github.com/golang/glog"
 )
 
-const PASSWORD_LENGTH = 16
+const passwordLength = 16
 
-type GeneratePassword func(length int) string
+type generatePassword func(length int) string
 
 type applicationService struct {
-	generatePassword     GeneratePassword
+	generatePassword     generatePassword
 	applicationDirectory application_directory.ApplicationDirectory
 }
 
-func New(generatePassword GeneratePassword, applicationDirectory application_directory.ApplicationDirectory) *applicationService {
+func New(generatePassword generatePassword, applicationDirectory application_directory.ApplicationDirectory) *applicationService {
 	s := new(applicationService)
 	s.generatePassword = generatePassword
 	s.applicationDirectory = applicationDirectory
@@ -25,38 +25,41 @@ func New(generatePassword GeneratePassword, applicationDirectory application_dir
 }
 
 func (s *applicationService) DeleteApplication(applicationName model.ApplicationName) error {
-	glog.V(2).Infof("delete application %v", applicationName)
+	glog.V(4).Infof("delete application %v", applicationName)
 	err := s.applicationDirectory.Delete(applicationName)
 	if err != nil {
 		glog.V(2).Infof("delete application %v failed: %v", applicationName, err)
 		return err
 	}
-	glog.V(2).Infof("deleted application %v successful", applicationName)
+	glog.V(4).Infof("deleted application %v successful", applicationName)
 	return nil
 }
 
 func (s *applicationService) ExistsApplication(applicationName model.ApplicationName) (bool, error) {
-	glog.V(2).Infof("exists application %v", applicationName)
+	glog.V(4).Infof("exists application %v", applicationName)
 	return s.applicationDirectory.Exists(applicationName)
 }
 
 func (s *applicationService) CreateApplication(applicationName model.ApplicationName) (*model.Application, error) {
-	glog.V(2).Infof("create application %v", applicationName)
-	applicationPassword := model.ApplicationPassword(s.generatePassword(PASSWORD_LENGTH))
+	glog.V(4).Infof("create application %v", applicationName)
+	applicationPassword := model.ApplicationPassword(s.generatePassword(passwordLength))
 	return s.createApplicationWithPassword(applicationName, applicationPassword)
 }
 
 func (s *applicationService) CreateApplicationWithPassword(applicationName model.ApplicationName, applicationPassword model.ApplicationPassword) (*model.Application, error) {
-	glog.V(2).Infof("create application with password %v", applicationName)
+	glog.V(4).Infof("create application with password %v", applicationName)
 	return s.createApplicationWithPassword(applicationName, applicationPassword)
 }
 
 func (s *applicationService) createApplicationWithPassword(applicationName model.ApplicationName, applicationPassword model.ApplicationPassword) (*model.Application, error) {
+	glog.V(4).Infof("create application %v with password-length %d", applicationName, len(applicationPassword))
 	exists, err := s.ExistsApplication(applicationName)
 	if err != nil {
+		glog.V(2).Infof("check application exists failed: %v", err)
 		return nil, err
 	}
 	if exists {
+		glog.V(2).Infof("applicaton %v already exists", applicationName)
 		return nil, fmt.Errorf("applicaton %v already exists", applicationName)
 	}
 	application := model.Application{
@@ -67,26 +70,30 @@ func (s *applicationService) createApplicationWithPassword(applicationName model
 		glog.V(2).Infof("create application %v failed: %v", applicationName, err)
 		return nil, err
 	}
-	glog.V(2).Infof("created application %v successful", applicationName)
+	glog.V(4).Infof("created application %v successful", applicationName)
 	return &application, nil
 }
 
 func (s *applicationService) VerifyApplicationPassword(applicationName model.ApplicationName, applicationPassword model.ApplicationPassword) (bool, error) {
-	glog.V(2).Infof("verify password of application %v", applicationName)
+	glog.V(4).Infof("verify password of application %v", applicationName)
 	pw, err := s.applicationDirectory.Get(applicationName)
 	if err != nil {
+		glog.V(2).Infof("get application %v failed: %v", applicationName, err)
 		return false, err
 	}
 	if *pw != applicationPassword {
+		glog.V(2).Infof("password for application %v invalid", applicationName)
 		return false, nil
 	}
+	glog.V(4).Infof("password for application %v valid", applicationName)
 	return true, nil
 }
 
 func (s *applicationService) GetApplication(applicationName model.ApplicationName) (*model.Application, error) {
-	glog.V(2).Infof("get application %v", applicationName)
+	glog.V(4).Infof("get application %v", applicationName)
 	applicationPassword, err := s.applicationDirectory.Get(applicationName)
 	if err != nil {
+		glog.V(2).Infof("get application %v failed: %v", applicationName, err)
 		return nil, err
 	}
 	application := model.Application{
